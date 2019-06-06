fa41626834413f0252fde7f57a284157e291e580
Leopoldo Hernandez <amnotme@icloud.com>
2019-06-05 23:04:39 -0700

    Updated main script to add control over degrees of aperture on drawing. Fixed initial constraint

--- LeoCapstone.py
+++ LeoCapstone.py
@@ -10,8 +10,8 @@ def run(context):
         #variables
         cirDiamR = 5; #specifies the diameter
         arcDist = cirDiamR / 2; #specifies the arcDistance
-        quantity = 3 #specifies the quantity of copies.
-     
+        replicas = 7 #specifies the quantity of copies.
+        degs = 30
         #setting app
         app = adsk.core.Application.get()
         ui  = app.userInterface
@@ -43,27 +43,37 @@ def run(context):
         #creating construction lines for x and y axis and constrains to the plane
         lineX = lines.addByTwoPoints(coreP(-cirDiamR,0,0), coreP(cirDiamR,0,0));
         lineY = lines.addByTwoPoints(coreP(0,-cirDiamR,0), coreP(0,cirDiamR,0));
+        lineXY = lines.addByTwoPoints(centerPoint, coreP(cirDiamR,cirDiamR,0))
+        
         lineX.isConstruction = True;
         lineY.isConstruction = True;
+        lineXY.isConstruction = True;
+        
+        #constraint construction lines
         constraints.addPerpendicular(lineX, lineY);
         constraints.addCoincident(centerPoint,lineX);
         constraints.addCoincident(centerPoint,lineY);
         constraints.addHorizontal(lineX);
+        
         dimensions.addDistanceDimension(lineX.startSketchPoint, lineX.endSketchPoint,orientations.HorizontalDimensionOrientation, coreP(3,3,0));
         dimensions.addDistanceDimension(lineY.startSketchPoint, lineY.endSketchPoint, orientations.VerticalDimensionOrientation, coreP(3, 4, 0));
+        dim = dimensions.addAngularDimension(lineY, lineXY, coreP(1,5,0));
+        dim.parameter.expression = "{} deg".format(degs);
         
         #creating initial circle
         innerCircle = circles.addByCenterRadius(centerPoint, cirDiamR);
         dimensions.addRadialDimension(innerCircle,coreP(3,5,0));
+        constraints.addCoincident(lineXY.endSketchPoint,innerCircle); 
         constraints.addCoincident(lineY.endSketchPoint,innerCircle);
         constraints.addCoincident(lineX.endSketchPoint, innerCircle);
-        
-        #creating initial arc
-        arc1 = arcs.addByThreePoints(centerPoint, coreP(-arcDist, arcDist, 0), coreP(0,cirDiamR,0));
-        constraints.addCoincident(arc1.startSketchPoint, innerCircle);          
-        constraints.addTangent(innerCircle, arc1);
        
-  
+        #creating initial arc
+        arc1 = arcs.addByThreePoints(centerPoint, coreP(-arcDist, arcDist, 0), lineXY.endSketchPoint);
+           
+        constraints.addCoincident(lineY.endSketchPoint, arc1);          
+
+       
+
         #disclosure:
         #       Fusion360 supports circular pattern feature with 3d models but not for sketches.
         #
@@ -76,23 +86,23 @@ def run(context):
         #       Downside to this is that the copies will not inherit or create new constraints and therefore you should
         #       think of another work around if you are solely working with the API instead of the GUI.
        
-       
-        #adding entities to use a quasi-sketch circularPattern feature.
+        # adding entities to use a quasi-sketch circularPattern feature.
+        # creates a collection of all objects.
         ent = adsk.core.ObjectCollection.create();
         ent.add(arc1);
-        
+        # we distinguish between the original sketch where we will make rotations from and then
+        # normalize it
         normal = currentSketch.xDirection.crossProduct(currentSketch.yDirection);
         normal.transformBy(currentSketch.transform);
         origin.transformBy(currentSketch.transform);
         rotationMatrix = adsk.core.Matrix3D.create();
-        step = 2 * math.pi / quantity
-        
-        for i in range(1, int(quantity)):
+        step = -1 * math.pi / replicas
+
+        for i in range(1, int(replicas + 1)):
             rotationMatrix.setToRotation(step * i, normal, origin)
             currentSketch.copy(ent, rotationMatrix);
-
+    
     except:
         if ui:
-            ui.messageBox('Failed but Rimma fails worse:\n{}'.format(traceback.format_exc()))
-
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
